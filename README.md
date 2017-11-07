c9c3e7d385bf6198e8941ea7d65c43160ab31ac9
rockyfire <rockyfire@126.com>
2017-11-07 16:40:50 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Crawler
 爬虫小程序
 
-[Scrapy爬取豆瓣电影TOP250]()
+[Scrapy爬取豆瓣电影TOP250](https://github.com/rockyfire/Crawler/blob/master/Scrapy%E7%88%AC%E5%8F%96%E8%B1%86%E7%93%A3%E7%94%B5%E5%BD%B1TOP250.md)
 
---
+++ crawler/crawler/pipelines.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import MySQLdb
+from scrapy.conf import settings
+from scrapy.contrib.pipeline.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
+
+class LoginPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+class MyImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        client_headers = settings['DEFAULT_REQUEST_HEADERS']
+        client_headers['Host'] = 'movie.douban.com'
+        for image_url in item['image_urls']:
+            yield Request(image_url)
+
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem("Item contains no images")
+        item['image_paths'] = image_paths
+        return item
